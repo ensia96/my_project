04f21cbcfc187199a8fa2747e169998c983629f0
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-04-07 17:59:03 +0900

    ~correct mistakes, make new app

--- westagram/westagram/settings.py
+++ westagram/westagram/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'comment'
+    'comment',
     # 'django.contrib.admin',
     # 'django.contrib.auth',
     'django.contrib.contenttypes',
