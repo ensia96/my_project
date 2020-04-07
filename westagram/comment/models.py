dc0cdb5d7cda30a41b6a7a8c5931e1f68697d649
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-04-07 17:25:51 +0900

    ~fix setting, make model

--- westagram/comment/models.py
+++ westagram/comment/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Comment(models.Model):
+    name       = models.CharField(max_length = 50)
+    contents   = models.TextField()
+    created    = models.DateTimeField(auto_now_add = True)
+    updated    = models.DateTimeField(auto_now = True)
--- westagram/westagram/urls.py
+++ westagram/westagram/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('comment/', include('westagram.urls')),
+    path('comment/', include('comment.urls')),
 ]
