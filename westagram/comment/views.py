e2dc968e010173794a92aaff939f57e96de2441f
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-04-07 17:30:09 +0900

    ~make view_logic for model

--- westagram/comment/views.py
+++ westagram/comment/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.views import View
+from django.http import HttpResponse, JsonResponse
+
+from .models import Comment
+
+class Leave_comment(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        Comment(
+            name       = data['name'],
+            contents   = data['contents']
+        ).save()
+        return JsonResponse({"Message":"APPLIED"}, status=200)
