7bbcb1d820d4250f3260e4d533ef5232c312b1e7
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-04-07 18:05:35 +0900

    ~apply new app, make table, import to view

--- westagram/userauth/models.py
+++ westagram/userauth/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Account(models.Model):
+    name       = models.CharField(max_length = 50)
+    password   = models.CharField(max_length = 400)
+    created    = models.DateTimeField(auto_now_add = True)
+    updated    = models.DateTimeField(auto_now = True)
--- westagram/westagram/urls.py
+++ westagram/westagram/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('comment/', include('comment.urls')),
+    path('userauth/', include('userauth.urls')),
 ]
