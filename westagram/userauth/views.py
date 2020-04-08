225792d4a4f5108370d25003a5994fcf9d0178b0
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-04-08 16:57:17 +0900

    ~reprogrammed

--- westagram/userauth/views.py
+++ westagram/userauth/views.py
@@ -6,33 +6,25 @@ from django.http import HttpResponse, JsonResponse
 from .models import Account
 
 class Sign_up(View):
-    def get(self, request):
+    def post(self, request):
         data = json.loads(request.body)
         if Account.objects.get(name=data['name']):
-            return 0
-
-    def post(self, request):
-        if self.get(self, request) :
+            return JsonResponse({'Message':'ALREADY_EXIST'}, status=400) # already exist
+        else :
             Account(
                 name       = get().data['name'],
                 password   = get().data['password']
             ).save()
             return JsonResponse(status=200) # welcome!
-        else :
-            return JsonResponse({'Message':'ALREADY_EXIST'}, status=401) # already exist
 
 class Sign_in(View):
-    def get(self, request):
+    def post(self, request):
         data = json.loads(request.body)
         if Account.objects.get(name=data['name']):
             userdata = Account.objects.get(name=data['name'])
             userpw   = userdata.password
             if userpw == data['password']:
-                return 1
-
-    def post(self, request):
-        if Sign_in.get(self, request):
-            return JsonResponse({'Message':'SUCCESS'}, status=200) # user exist, pw match
-                # pw unmatch 는 많은 정보를 제공하기 때문에 보안을 위해 구현 x
+                return JsonResponse({'Message':'SUCCESS'}, status=200) # user exist, pw match
+                    # pw unmatch 는 많은 정보를 제공하기 때문에 보안을 위해 구현 x
         else:
             return JsonResponse({'Message':'INVALID_USER'}, status=401) # username unmatch
