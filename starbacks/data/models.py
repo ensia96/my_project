a2495081e21acebcbb4584a60fdd8ba642c01ad0
최호준 <ensia96@choehojunui-MacBook-Pro.local>
2020-04-11 21:25:02 +0900

    ~starbacks modeling before test

--- starbacks/data/models.py
+++ starbacks/data/models.py
@@ -1,41 +1,38 @@
 from django.db import models
 
 class Category(models.Model): # 음료, 푸드 등 카테고리 ( 대분류 ) 에 대한 분류를 위한 카테고리테이블
-    name=models.CharField(max_length=20)
-
-# 카테고리 항목 1개당 제품군 여러가지
+    name        = models.CharField(max_length=20)
 
 class Group(models.Model): # 콜드브루, 브루드커피 등 제품군 ( 중분류 ) 에 대한 분류가 이루어지는 제품군테이블
-    name=models.CharField(max_length=20)
+    name        = models.CharField(max_length=20)
+    category_id = models.ForeignKey(Category, on_delete = models.CASCADE)
 
-# 제품군 항목 1개당 제품 여러가지
+class Product(models.Model): # 관리할 핵심데이터인 각종 제품들 ( 세분류 ) 에 대한 분류가 이루어지는 제품테이블
+    name_ko     = models.CharField(max_length=50)
+    name_en     = models.CharField(max_length=50)
+    group_id    = models.ForeignKey(Group, on_delete = models.CASCADE)
 
-class Product(models.Model): # 각종 메뉴들 ( 관리 데이터 단위 ) 에 대한 분류가 이루어지는 제품테이블
-    name_ko=models.CharField(max_length=50)
-    name_en=models.CharField(max_length=50)
+class Product_Allergy(models.Model): # 제품 - 알레르기 연결
+    product_id  = models.ForeignKey(Product, on_delete = models.CASCADE)
+    allergy_id  = models.ForeignKey(Allergy, on_delete = models.CASCADE)
 
+class Allergy(models.Model): # 1 제품당, 0 ~ 알레르기
+    name        = models.CharField(max_length=20)
 
+class Description(models.Model): # 1 제품당, 0 ~ 2 설명
+    desc_top    = models.TextField(blank=True)
+    desc_btm    = models.TextField(blank=True)
 
-# 1 제품당 0 ~ 여러 알레르기
+class Ingredient(models.Model): # 1 제품당, 1 영양정보
+    size_id     = models.CharField(max_length=20)
+    kcal        = models.FloatField()
+    sugar       = models.FloatField()
+    protein     = models.FloatField()
+    sodium      = models.FloatField()
+    fat         = models.FloatField()
+    caffeine    = models.FloatField()
 
-class Allergy(models.Model): # 알레르기 품목 나열
-    name=models.CharField(max_length=20)
-
-
-
-class Description(models.Model): # 제품 1개당 상세설명 1 row
-    desc_top=models.TextField()
-    desc_btm=models.TextField()
-
-
-
-class Ingredient(models.Model): # 제품 1개당 영양정보 1 row
-    name=models.CharField(max_length=20)
-
-
-class Ammount(models.Model): # 영양정보 1개당 기준량 1 row
-    name=models.CharField(max_length=20)
-    num=models.IntegerField
-
-
-# models.ForeignKey(클래스, on_delete = models.CASCADE)
+class Size(models.Model): # 1 영양정보당, 1 사이즈 ( 기준용량 )
+    name        = models.CharField(max_length=20)
+    num_ml      = models.IntegerField()
+    num_oz      = models.IntegerField()
