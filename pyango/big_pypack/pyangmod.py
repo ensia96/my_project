576eb465158b0831e7e1df1885f29c5f6b61e4c9
최호준 <ensia96@gmail.com>
2020-04-15 15:50:49 +0900

    ~try set, hash replit

--- pyango/big_pypack/pyangmod.py
+++ pyango/big_pypack/pyangmod.py
@@ -1,9 +1,16 @@
-# 파이 패키지의 퍙 모듈입니다!
+import hashlib
 
-def pythonmang() : # 파이썬망이라는 함수를 정의할게요.
-    print("mango is pymaster")
-    # 출력해주세요. "망고는 파이마스터" 라고 해주세요.
+def sha1_hash(input_str):
+    
+    hash_obj = hashlib.sha1(input_str.encode())
+    hash_value = hash_obj.hexdigest()
+    return hash_value
 
-onlypyang = ["no!", "i'm in the mod!"]
-# 온리퍙이라는 변수를 정의할게요.
-# "안돼!", "난 모듈에 있단말이야!" 라는 내용이 담긴 리스트에요.
+
+wecode_hash_value = sha1_hash('wecode')
+print(wecode_hash_value)
+print(len(wecode_hash_value))
+
+hash_value_1234 = sha1_hash('1234')
+print(hash_value_1234)
+print(len(hash_value_1234))
--- pyango/small_pypack/pengmod.py
+++ pyango/small_pypack/pengmod.py
@@ -1,26 +1,45 @@
-class MySet:
-  def __init__(self, size):
-    self.size = size
-    self.bucket = [ None for x in range(size) ] # => [ None 의 개수가 size 만큼 있다. ]
+import hashlib
+
+class MySet: # 내가 지정할 암호화방식 ( 몇 개의 버킷을 사용할 지 )
+  def __init__(self, size): # class 의 size 인자를 받아 이니셜
+    self.size = size # 인자로 받은 값으로 size 지정
+    self.bucket = [ None for x in range(size) ] # 자정한 size 수 만큼 None(buacket)을 담은 list 생성
 
   def values(self):
-    set_data =""
-    for i in range(self.size):
-      if self.bucket[i] != None:
-        set_data += self.bucket[i]
-        set_data += ", "
-    if set_data[-1] == " ":
-      data_set = set_data[:-2]
-    return data_set
+    set_data ="" # 정보를 담을 set_data (기준 문자열) 생성
+    for i in range(self.size): # size 범위에 대해 반복구문
+      if self.bucket[i] != None: # list 의 원소들에 대해 None 이 아닌 경우의 조건구문
+        set_data += self.bucket[i] # 기준 문자열에 list 의 원소 추가
+        set_data += ", " # 다음에 올 원소와의 구분자 추가
+    if set_data[-1] == " ": # 반복이 끝난 후의 조건구문
+      data_set = set_data[:-2] # 생성된 기준 문자열을 data_set 에 할당
+    return data_set # 마지막 ', ' 부분이 사라진 문자열 반환
 
-  # 1. hash_value 함수
-  def hash_value(self,key):
-    hash_value = hashlib.sha1(key.encode())
+# 1. hash_value 함수 : 이 함수는 
+  def hash_value(self,key): # sha1 방식으로 구한 해시값을 16진수화 하여, 숫자형 데이터만 추출한 뒤, 버킷수로 나눈 나머지값을 출력
+    hash_value = hashlib.sha1(key.encode())# 인자로 받은 key 를 인코드하고 sha1 함수로 작업
     # 여기서 부터 구현
-  
-  # 2. add 함수
+    base = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] # 비교대상 list 지정 ( 16진수는 숫자모양도 문자형 )
+    ineed="" # 정보를 담을 ineed (기준 문자열) 생성
+    for t in hash_value.hexdigest(): # sha1 방식으로 암호화된 값을 16진수화한 상태의 요소들에 대한 반복구문
+      if t in base: # 비교대상 list 와의 비교를 통한 조건 구문
+        ineed += t # 조건 만족값들을 기준 문자열에 추가
+    return int(ineed) % self.size # 추출된 기준 문자열을 정수화한 뒤 class 의 size 값으로 나눈 나머지를 return
+
+# 2. add 함수
   def add(self, key):
-    hash_value = self.hash_value(key)
+    hash_value = self.hash_value(key) # 같은 class 의 함수내용 호출
     # 여기서 부터 구현
+    # self.bucket[hash_value]={key}
+    if self.bucket[hash_value]==None:
+      self.bucket[hash_value]={key}
+      return self.bucket
+    if key in self.bucket[hash_value]:
+      self.bucket[hash_value].add(key)
+      return self.bucket
 
-print(MySet)
+a = MySet(15)
+#a.add('wecode')
+c = a.add('weplay') 
+
+print(c)
