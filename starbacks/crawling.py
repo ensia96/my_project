86e3ecbaad6af920c7e23070c6f5beede4a376c6
최호준 <ensia96@gmail.com>
2020-04-17 11:23:03 +0900

    ~made crawling decorator

--- starbacks/crawling.py
+++ starbacks/crawling.py
@@ -7,28 +7,29 @@ options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 options.add_argument("disable-gpu")
 # 혹은 options.add_argument("--disable-gpu")
-driver = webdriver.Chrome('/Users/ensia96/Documents/chromedriver', options=options)
+driver = webdriver.Chrome('/Users/ensia96/Documents/mydocs/chromedriver', options=options)
 driver.implicitly_wait(1)
-#driver.get('https://www.starbucks.co.kr/menu/drink_list.do') # starbuck url 접근
-#bs = BeautifulSoup(driver.page_source, 'html.parser')
 
-# 음료 카테고리의 제품군 list 리턴
+def get_source(i): # 소스 받아오는 데코레이터  'https://www.starbucks.co.kr/menu/drink_list.do'
+    def deco_func(func):
+        def get():
+            driver.get(i) # starbuck url 접근
+            bs = BeautifulSoup(driver.page_source, 'html.parser')
+            return func(bs)
+        return get
+    return deco_func
 
-def group_craw():
-    driver.get('https://www.starbucks.co.kr/menu/drink_list.do') # starbuck url 접근
-    bs = BeautifulSoup(driver.page_source, 'html.parser')
+@get_source('https://www.starbucks.co.kr/menu/drink_list.do')
+def group_craw(bs): # 음료 카테고리의 제품군 list 리턴
     target = bs.findAll('label',{'for':re.compile('product_*')})
     group_name = []
     for group in target:
         group_name.append(group.text)
-    return group_name[1:] # ['콜드 브루 커피', '브루드 커피', '에스프레소', '프라푸치노', '블렌디드', '스타벅스 피지오', '티(티바나)', '기타 제조 음료', '스타벅스 주스(병음료)']
+    return group_name[1:]
 
 
-# 제품명, 영양정보 list 리턴
-
-def product_name_ingredient_craw():
-    driver.get('https://www.starbucks.co.kr/menu/drink_list.do') # starbuck url 접근
-    bs = BeautifulSoup(driver.page_source, 'html.parser')
+@get_source('https://www.starbucks.co.kr/menu/drink_list.do')
+def product_name_ingredient_craw(bs):# 제품명, 영양정보 list 리턴
     target = bs.findAll('td')
 
     product = []
@@ -57,26 +58,32 @@ def product_name_ingredient_craw():
             caffeine.append(product_name)
         i += 1
 
+@get_source('https://www.starbucks.co.kr/menu/drink_list.do')
+def mk_prod_list(func): # 제품 id list 를 돌려주는 데코레이터
+    def prod_id(bs):
+        target = bs.findAll('a', {'class':'goDrinkView'})
+        product_id=[]
+        for i in range(len(target)):
+            product_id.append(target[i]['prod'])
+        return func(product_id)
+    return prod_id
 
-driver.get('https://www.starbucks.co.kr/menu/drink_list.do')
-bs = BeautifulSoup(driver.page_source, 'html.parser')
-target = bs.findAll('a', {'class':'goDrinkView'})
-link_list=[]
-for i in range(len(target)):
-    link_list.append(target[i]['prod'])
+#@mk_prod_list
+#def test(i):
+#    print(i)
 
 
-name_en = []
-description = []
-for link in link_list:
-    driver.get(f'https://www.starbucks.co.kr/menu/drink_view.do?product_cd={link}')
-    soe = BeautifulSoup(driver.page_source, 'html.parser')
-    desc_top = driver.find_element_by_class_name('t1')
-    desc_btm = driver.find_element_by_class_name('product_view_wrap2')
-    description.append(desc_top)
-    description.append(desc_btm)
-
-print(description)
+#name_en = []
+#description = []
+#for ids in product_id:
+#    driver.get(f'https://www.starbucks.co.kr/menu/drink_view.do?product_cd={ids}')
+#    soe = BeautifulSoup(driver.page_source, 'html.parser')
+#    desc_top = driver.find_element_by_class_name('t1')
+#    desc_btm = driver.find_element_by_class_name('product_view_wrap2')
+#    description.append(desc_top.text)
+#    description.append(desc_btm.text)
+#
+#print(len(description))
 
 
 
