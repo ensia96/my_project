732e02e8e5bd6621793188fc71b6df861699662f
최호준 <ensia96@gmail.com>
2020-05-22 02:35:32 +0900

    node project

--- study_node/routes/users.js
+++ study_node/routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const userController = require('../controllers/user');
+
 const router = express.Router();
 
-router.get('/:id', (req, res, next) => {})
-
-router.post('/', (req, res, next) => {})
+//router.get('/:id', userController.readUserFile)
+router.post('/', userController.createUser)
+router.post('/login', userController.login)
 
 module.exports = router;
--- study_node/server.js
+++ study_node/server.js
@@ -1,8 +1,14 @@
-const http = require("http");
-const app = require("./app");
+require('dotenv').config({});
+const http = require('http');
+const app = require('./app');
+const { connect } = require('./db');
 
 const server = http.createServer(app);
 
 server.listen(8000, () => {
-  console.log("mangoserver is coming");
+  // console.log(process.env.foo)
+  console.log('mango is coming');
+  connect(() => {
+    console.log('mongo is coming');
+  });
 });
